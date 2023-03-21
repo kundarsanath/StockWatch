5f42ba3d5b87423e02e7840548a163c1c714fc6d
Prashu <Prashanth.P@stackroute.in>
2023-03-21 11:16:46 +0530

    stock app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HomeComponent } from './home/home.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { TimedComponent } from './timed/timed.component';
+import {MatCardModule} from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomeComponent,
+    WishlistComponent,
+    TimedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // BrowserAnimationsModule to display animations
+    BrowserAnimationsModule,
+    // MatButtonModule to display buttons
+    MatButtonModule,
+    // FlexLayoutModule to display in flex layout
+    FlexLayoutModule,
+    // MatCardModule to dispaly cards
+    MatCardModule,
+    // HttpClientModule to make http requests
+    HttpClientModule,
+    // MatSnackBarModule to display snack bar
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
